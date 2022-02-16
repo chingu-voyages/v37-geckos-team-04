2404a05b787444a3352ceb7ff83a7f0897c156e4
Jenny Kim <kimj1905@gmail.com>
2022-02-16 15:51:23 -0500

    added flexbox and basic styling to main content on landing page

--- frontend/src/Components/03-LandingPage/style.js
+++ frontend/src/Components/03-LandingPage/style.js
@@ -1,3 +1,20 @@
 import styled from 'styled-components';
 
-export const MainContent = styled.div``;
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 2em;
+  padding: 10em;
+
+  div {
+    font-size: 3rem;
+  }
+
+  button {
+    font-size: 2.25rem;
+    /* width: 20%; */
+    text-align: center;
+    padding: 0.5em;
+  }
+`;
