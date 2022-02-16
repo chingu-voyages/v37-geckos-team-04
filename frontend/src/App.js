1ea3c16af08c627902a5c6562041b9af2f646ee2
Jenny Kim <kimj1905@gmail.com>
2022-02-16 14:30:10 -0500

    added basic text for landing page's main content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './Components/01-Navbar/Navbar';
 import Footer from './Components/02-Footer/Footer';
+import LandingPage from './Components/03-LandingPage/LandingPage';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <LandingPage />
       <Footer />
     </div>
   );
---
+++ frontend/src/Components/03-LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { MainContent } from './style';
+
+export default function LandingPage() {
+  return (
+    <MainContent>
+      <div>Call to action slogan here.</div>
+      <button>Get Started</button>
+    </MainContent>
+  );
+}
