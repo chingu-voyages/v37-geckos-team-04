eecf9385f30443dabd0bba9448ca0e75d5e60642
Jenny Kim <kimj1905@gmail.com>
2022-02-16 15:31:34 -0500

    removed default margin and padding for entire body of html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { GlobalStyle } from './AppStyle';
 import Navbar from './Components/01-Navbar/Navbar';
 import Footer from './Components/02-Footer/Footer';
 import LandingPage from './Components/03-LandingPage/LandingPage';
@@ -5,6 +6,7 @@ import LandingPage from './Components/03-LandingPage/LandingPage';
 export default function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Navbar />
       <LandingPage />
       <Footer />
