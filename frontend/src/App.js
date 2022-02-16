ab246e479a27e3594cbfb783917d9c124bdfcab1
Jenny Kim <kimj1905@gmail.com>
2022-02-16 14:26:53 -0500

    added footer component folder and files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './Components/01-Navbar/Navbar';
+import Footer from './Components/02-Footer/Footer';
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
