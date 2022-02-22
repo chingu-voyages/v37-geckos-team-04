b4ea647939b73bd30a11525eb07bbd68a16a7b27
Jenny Kim <kimj1905@gmail.com>
2022-02-21 19:40:29 -0500

    imported navbar and footer componentsn into landing page component instead

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,11 @@
 import { GlobalStyle } from './AppStyle';
-import Navbar from './Components/01-Navbar/Navbar';
-import Footer from './Components/02-Footer/Footer';
 import LandingPage from './Components/03-LandingPage/LandingPage';
 
 export default function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <Navbar />
       <LandingPage />
-      <Footer />
     </div>
   );
 }
--- frontend/src/Components/03-LandingPage/LandingPage.js
+++ frontend/src/Components/03-LandingPage/LandingPage.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { MainContent } from './style';
+import Navbar from '../01-Navbar/Navbar';
+import Footer from '../02-Footer/Footer';
 
 export default function LandingPage() {
   return (
-    <MainContent>
-      <div>Call to action slogan here.</div>
-      <button>Get Started</button>
-    </MainContent>
+    <>
+      <Navbar />
+      <MainContent>
+        <div>Call to action slogan here.</div>
+        <button>Get Started</button>
+      </MainContent>
+      <Footer />
+    </>
   );
 }
