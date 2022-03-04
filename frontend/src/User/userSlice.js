0ae593b88f46eeb1c3bbdd3b8270c27269eee345
Jenny Kim <kimj1905@gmail.com>
2022-03-04 13:11:37 -0500

    initial set up of userSlice for the reducers and action creators

--- frontend/src/User/userSlice.js
+++ frontend/src/User/userSlice.js
@@ -1,9 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {};
+const initialState = {
+  user: null,
+  isLoggedIn: false,
+};
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {},
+  reducers: {
+    signUpSuccess: (state) => {
+      state.user;
+      //   need to figure out what to configure state.user to;
+      state.isLoggedIn = true;
+    },
+    signUpFailure: (state) => {
+      //   state.isLoggedIn = false;
+    },
+    logInSuccess: (state) => {
+      state.user;
+      state.isLoggedIn = true;
+    },
+    logInFailure: (state) => {
+      //   state.isLoggedIn = false;
+    },
+  },
 });
+
+export const { signUpSuccess, signUpFailure, logInSuccess, logInFailure } =
+  userSlice.actions;
+
+export default userSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './User/userSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
