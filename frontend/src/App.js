b49a915a5dd385ed8ac526148659e185909f4ea3
Jenny Kim <kimj1905@gmail.com>
2022-03-04 13:31:40 -0500

    set up private dashboard route and created Dashboard component folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,33 @@
 import { GlobalStyle } from './style';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
+
 import LandingPage from './Components/04-LandingPage/LandingPage';
 import LogInPage from './Components/05-LoginSignUpPage/LogInPage';
 import SignUpPage from './Components/05-LoginSignUpPage/SignUpPage';
+import Dashboard from './Components/06-Dashboard/Dashboard';
 
 export default function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/login" element={<LogInPage />}></Route>
-        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LogInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="" element={<Dashboard />} />
+        </Route>
       </Routes>
-      {/* <LandingPage />
-      <LogInPage />
-      <SignUpPage /> */}
     </div>
   );
+}
+
+function PrivateRoute() {
+  const auth = useAuth();
+  return auth ? <Outlet /> : <Navigate to="/login" />;
+}
+
+function useAuth() {
+  // For now
+  return true;
 }
