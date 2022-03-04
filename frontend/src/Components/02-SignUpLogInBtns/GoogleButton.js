47168d4d024b663bfca38a408feaeb11acbb9dee
Ezekiel <umanmah234@gmail.com>
2022-03-03 19:32:09 -0500

    refactor <GoogleButton />

--- frontend/src/Components/02-SignUpLogInBtns/GoogleButton.js
+++ frontend/src/Components/02-SignUpLogInBtns/GoogleButton.js
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import GoogleLogin, { GoogleLogout } from 'react-google-login';
+import GoogleLogin from 'react-google-login';
 import { GoogleLogo } from './style';
 
-const initialState = { firstName: '', lastName: '', isLoggedIn: false };
-
 const GoogleButton = () => {
-  const [user, setUser] = useState(initialState);
-
   const googleSuccess = async (res) => {
     const result = res?.profileObj;
     const token = res?.tokenId;
+
+    // This request decodes the token from google; this will be later modified to have the backend decode the token
+    await fetch(
+      `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`,
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      }
+    ).then((res) => console.log('res: ', res));
 
     // const data = {
     //   token,
@@ -27,23 +32,14 @@ const GoogleButton = () => {
     // }).then((response) => console.log('fetch res: ', response));
 
     // Dispatch an auth action to the store to store result and token and push history to desired page
-
-    setUser({
-      firstName: result.givenName,
-      lastName: result.familyName,
-      isLoggedIn: true,
-    });
   };
 
   const googleFailure = () =>
     console.log('Sign in with Google was unsuccessful. Try again later.');
 
-  const logout = () => setUser(initialState);
-
   return (
     <GoogleLogin
       clientId="213058889101-r9cesh30eb5g2tftmld264gko9m6gltl.apps.googleusercontent.com"
-      buttonText="Login with Google"
       onSuccess={googleSuccess}
       onFailure={googleFailure}
       cookiePolicy={'single_host_origin'}
