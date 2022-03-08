6fd8114c7cbeb03ee78a66bd928f225610df3939
Jenny Kim <kimj1905@gmail.com>
2022-03-08 15:03:42 -0500

    revised user action creators and started to connect backend with frontend

--- frontend/src/reducers/User.js
+++ frontend/src/reducers/User.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { isAuthenticated, logOutSuccess } from './userSlice';
+// import React from 'react';
+import { isAuthenticated } from './userSlice';
 
-export const User = () => {
-  // const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const dispatch = useDispatch();
-
-  // return dispatch(logInSuccess());
+export const logIn = (formData, history) => async (dispatch) => {
+  try {
+    const { data } = await api.logIn(formData);
+    dispatch(isAuthenticated({ data }));
+    // change path name to dashboard, once the private route for the dashboard is set up
+    history.push('/');
+  } catch (error) {
+    alert(`Email or password is incorrect. Please try again.`);
+  }
 };
+
+export const signUp = (formData, history) => async (dispatch) => {
+  try {
+    const { data } = await api.signUp(formData);
+    dispatch(isAuthenticated({ data }));
+    history.push('/');
+  } catch (error) {
+    // More specific error handling for signing up.
+    // alert(`Email or password is incorrect. Please try again.`);
+  }
+};
+
+// dispatch user log out action directly from the log out button
