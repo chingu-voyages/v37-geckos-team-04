cbbefeff86165463709870c951a81aafa741b205
Jenny Kim <kimj1905@gmail.com>
2022-03-07 19:41:53 -0500

    added another container to landing page and changed flexbox to css grid for styling

--- frontend/src/Components/04-LandingPage/LandingPage.js
+++ frontend/src/Components/04-LandingPage/LandingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LandingPageCont, MainContent } from './style';
+import { LandingPageCont, MainContent, MainContentCont } from './style';
 import Header from '../00-Header/Header';
 import Footer from '../03-Footer/Footer';
 
@@ -7,10 +7,15 @@ export default function LandingPage() {
   return (
     <LandingPageCont>
       <Header />
-      <MainContent>
-        <div>Track your sleeping pattern with 'Sleep Tracker App Name'</div>
-        <button>Get Started</button>
-      </MainContent>
+      <MainContentCont>
+        <MainContent>
+          <div></div>
+          <div className="call-to-action">
+            Track your sleeping pattern with 'Sleep Tracker App Name'
+            <button>Get Started</button>
+          </div>
+        </MainContent>
+      </MainContentCont>
       <Footer />
     </LandingPageCont>
   );
--- frontend/src/Components/04-LandingPage/style.js
+++ frontend/src/Components/04-LandingPage/style.js
@@ -11,34 +11,55 @@ export const LandingPageCont = styled.section`
   background-size: cover;
 `;
 
-export const MainContent = styled.main`
+export const MainContentCont = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 2em;
-  justify-content: center;
   flex-grow: 1;
+`;
 
-  div {
+export const MainContent = styled.main`
+  /* display: flex;
+  flex-direction: row; */
+  /* align-items: flex-start; */
+  /* row-gap: 2em; */
+  /* justify-content: center;
+  flex-grow: 1; */
+  /* width: 35rem; */
+
+  /* div {
+    color: white;
+  } */
+  display: grid;
+  grid-template-areas:
+    '. . .'
+    '. main .'
+    '. . .'
+    '. . .';
+  grid-template-columns: 0.5fr 1.5fr 1fr;
+  grid-template-rows: repeat(4, 1fr);
+
+  .call-to-action {
     color: white;
     font-size: 2rem;
-  }
+    grid-area: main;
+    font-weight: 500;
 
-  button {
-    font-size: 1.75rem;
-    text-align: center;
-    padding: 0.5em 1.125em 0.5em 1.125em;
-    border-radius: 40px;
-    border: none;
-    /* background-color: #96c5f7; */
-    background-color: #eca72c;
-    color: white;
-    /* color: #f2f4ff; */
-    /* border: 2px solid #96c5f7;
-    background-color: #f2f4ff;
-    color: #96c5f7; */
-    /* font-family: 'Roboto', sans-serif; */
-    font-family: 'Montserrat', sans-serif;
-    font-weight: bold;
+    button {
+      font-size: 1.75rem;
+      text-align: center;
+      padding: 0.5em 1.125em 0.5em 1.125em;
+      border-radius: 40px;
+      border: none;
+      /* background-color: #96c5f7; */
+      background-color: #eca72c;
+      color: white;
+      /* color: #f2f4ff; */
+      /* border: 2px solid #96c5f7;
+      background-color: #f2f4ff;
+      color: #96c5f7; */
+      /* font-famil: 'Roboto', sans-serif; */
+      margin-top: 2rem;
+      font-family: 'Montserrat', sans-serif;
+      font-weight: bold;
+    }
   }
 `;
