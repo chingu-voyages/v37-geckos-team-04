a4d4f4c44ae7be11fabc30a2f1036c082035cb85
Ezekiel <umanmah234@gmail.com>
2022-03-10 15:28:47 -0500

    remodel userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
 
-import sleepDataSchema from './sleepData.js';
-
 const userSchema = mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
   id: String,
-  // sleepData: [sleepDataSchema],
 });
 
 export default mongoose.model('User', userSchema);
