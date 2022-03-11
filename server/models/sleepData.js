fc63aef13eb789ae540293da3a227d69403d4bc7
Ezekiel <umanmah234@gmail.com>
2022-03-11 09:49:50 -0500

    remodel sleep schema

--- server/models/sleepData.js
+++ server/models/sleepData.js
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 
+// sleepStart/sleepEnd should just store the time
+//
+
 const sleepDataSchema = mongoose.Schema({
-  start: Date,
-  end: Date,
-  duration: Number,
+  sleepStart: { type: Date, default: Date.now() },
+  sleepEnd: { type: Date, default: Date.now() },
+  date: Date,
+  mood: Number,
+  sleepGoal: Number,
+  notes: [String],
 });
 
 export default sleepDataSchema;
