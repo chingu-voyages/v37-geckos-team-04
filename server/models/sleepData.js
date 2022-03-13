bd098550ddaec3610f8089fa4fbb5c3ad16ae238
Ezekiel <umanmah234@gmail.com>
2022-03-13 19:23:43 -0400

    reformat sleep schema

--- server/models/sleepData.js
+++ server/models/sleepData.js
@@ -1,15 +1,17 @@
 import mongoose from 'mongoose';
 
 // sleepStart/sleepEnd should just store the time
-//
+// creator is the id of the person's profile id creating the sleep instance
 
 const sleepDataSchema = mongoose.Schema({
+  creator: String,
   sleepStart: { type: Date, default: Date.now() },
-  sleepEnd: { type: Date, default: Date.now() },
-  date: Date,
-  mood: Number,
+  sleepEnd: { type: Date, default: null },
+  date: { type: Date, default: Date.now() },
+  moodStart: Number,
+  moodWake: Number,
   sleepGoal: Number,
   notes: [String],
 });
 
-export default sleepDataSchema;
+export default mongoose.model('SleepData', sleepDataSchema);
