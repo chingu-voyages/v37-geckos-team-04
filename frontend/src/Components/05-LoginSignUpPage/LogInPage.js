8349a449571aa376dc94eba11ef2126b3d716bf5
Ezekiel <umanmah234@gmail.com>
2022-03-15 19:27:32 -0400

    reformat react-router-dom imports

--- frontend/src/Components/05-LoginSignUpPage/LogInPage.js
+++ frontend/src/Components/05-LoginSignUpPage/LogInPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 
 import { logInAsync } from '../../reducers/User';
 
