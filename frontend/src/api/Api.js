798a36ad120d5dcf673128e9fb538dc68c89b033
Ezekiel <umanmah234@gmail.com>
2022-03-20 09:28:58 -0400

    add sleep API routes

--- frontend/src/api/Api.js
+++ frontend/src/api/Api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
-// const axios = require('axios');
 
 const url = 'http://localhost:4000/users';
-// const url = 'http://localhost:4000';
 
-// export const login = async (formData) => axios.post(`${url}/login`, formData);
 export const login = (formData) => axios.post(`${url}/login`, formData);
-// export const signup = async (formData) => axios.post(`${url}/signup`, formData);
 export const signup = (formData) => axios.post(`${url}/signup`, formData);
+
+export const getSleepData = (id) => axios.get(`${url}/sleepData/${id}`);
+// prettier-ignore
+export const createSleep = (formData) => axios.post(`${url}/sleepData`, formData);
+// prettier-ignore
+export const updateSleep = (id, formData) => axios.patch(`${url}/sleepData/${id}`, formData);
+export const removeSleep = (id) => axios.delete(`${url}/sleepData/${id}`);
