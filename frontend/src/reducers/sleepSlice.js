2d883de3797097491fc5da85d5e125d0515a7f4b
Ezekiel <umanmah234@gmail.com>
2022-03-20 09:28:40 -0400

    add sleep reducers

--- frontend/src/reducers/sleepSlice.js
+++ frontend/src/reducers/sleepSlice.js
@@ -1,11 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const sleepSlice = createSlice({
-  name: 'sleepData',
-  initialState: {},
-  reducers: {},
+  name: 'sleep',
+  initialState: { data: [] },
+  reducers: {
+    get: (state, action) => {
+      state.data = action.payload;
+    },
+    create: (state, action) => {
+      state.data.push(action.payload);
+    },
+    update: (state, action) => {
+      let sleepInstance = state.data.find(
+        (sleep) => sleep._id === action.payload._id
+      );
+      if (sleepInstance) {
+        return { ...state, ...action.payload };
+      }
+    },
+    remove: (state, action) => {
+      state.data.filter((sleep) => sleep._id === action.payload._id);
+    },
+  },
 });
 
-// export const {} = sleepSlice.actions;
+export const { get, create, update, remove } = sleepSlice.actions;
 
 export default sleepSlice.reducer;
