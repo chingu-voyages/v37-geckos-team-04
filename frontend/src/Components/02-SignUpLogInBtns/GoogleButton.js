2ce0810bfebaf2f06c2672783e55b69f0bfd356c
Ezekiel <umanmah234@gmail.com>
2022-03-22 16:15:40 -0400

    dispatch auth action on login success

--- frontend/src/Components/02-SignUpLogInBtns/GoogleButton.js
+++ frontend/src/Components/02-SignUpLogInBtns/GoogleButton.js
@@ -1,50 +1,21 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import GoogleLogin from 'react-google-login';
 import { GoogleLogo } from './style';
 import { isAuthenticated } from '../../reducers/userSlice';
 
-// import { useLocation } from 'react-router-dom';
-
 const GoogleButton = () => {
-  // const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const googleSuccess = async (res) => {
     const result = res?.profileObj;
     const token = res?.tokenId;
     dispatch(isAuthenticated({ data: result, token }));
-    // This request decodes the token from google; this will be later modified to have the backend decode the token
-    // await fetch(
-    //   `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${token}`,
-    //   {
-    //     method: 'GET',
-    //     headers: { 'Content-Type': 'application/json' },
-    //   }
-    // ).then((res) => console.log('res: ', res));
-    // navigate('/dashboard');
-    // const data = {
-    //   token,
-    //   email: result.email,
-    //   password: 'password',
-    // };
-    // console.log('google response: ', res);
-    // await fetch('http://localhost:4000/users/login', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({ token }),
-    // }).then((response) => console.log('fetch res: ', response));
-
-    // Dispatch an auth action to the store to store result and token and push history to desired page
   };
 
   const googleFailure = () =>
     console.log('Sign in with Google was unsuccessful. Try again later.');
-  // const location = useLocation();
 
   return (
     <GoogleLogin
