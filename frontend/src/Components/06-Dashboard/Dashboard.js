8879f624aee60bc206076ba431f65ad9c8ce7099
Ezekiel <umanmah234@gmail.com>
2022-03-23 08:53:18 -0400

    re-add logout button function to logout button

--- frontend/src/Components/06-Dashboard/Dashboard.js
+++ frontend/src/Components/06-Dashboard/Dashboard.js
@@ -35,33 +35,37 @@ export default function Dashboard() {
   };
 
   return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={collapsed => setCollapsed(collapsed)}>
-          <Menu theme="dark" defaultSelectedKeys={['graphs']} mode="inline">
-            <Menu.Item key="graphs" icon={<PieChartOutlined />}>
-              <Link to="graphs">Dashboard</Link>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(collapsed) => setCollapsed(collapsed)}
+      >
+        <Menu theme="dark" defaultSelectedKeys={['graphs']} mode="inline">
+          <Menu.Item key="graphs" icon={<PieChartOutlined />}>
+            <Link to="graphs">Dashboard</Link>
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<DesktopOutlined />} title="Preferences">
+            <Menu.Item key="2" icon={<DashboardOutlined />}>
+              SleepGoal <Input min={4} max={12} defaultValue={8}></Input>
             </Menu.Item>
-            <SubMenu key="sub1" icon={<DesktopOutlined />} title="Preferences">
-              <Menu.Item key="2" icon={<DashboardOutlined />}>
-                SleepGoal        <Input min={4} max={12} defaultValue={8}></Input>
-              </Menu.Item>
-              {/* <Menu.Item key="3" icon={<EyeInvisibleOutlined />}>Bed-Time</Menu.Item>
+            {/* <Menu.Item key="3" icon={<EyeInvisibleOutlined />}>Bed-Time</Menu.Item>
               <Menu.Item key="4" icon={<EyeOutlined />}>Wake-Time</Menu.Item> */}
-            </SubMenu>
-            <Menu.Item key="history" icon={<HistoryOutlined />}>
-              <Link to="history">History</Link>
-            </Menu.Item>
-            <Menu.Item key="6" icon={<ArrowRightOutlined />}>
-              Log Out
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Modal />
-          <Content style={{ margin: '25px 35px' }}>
-              <Outlet />  
-          </Content>
-        </Layout>
+          </SubMenu>
+          <Menu.Item key="history" icon={<HistoryOutlined />}>
+            <Link to="history">History</Link>
+          </Menu.Item>
+          <Menu.Item key="6" icon={<ArrowRightOutlined />} onClick={logOutUser}>
+            Log Out
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Modal />
+        <Content style={{ margin: '25px 35px' }}>
+          <Outlet />
+        </Content>
       </Layout>
-  )
-} 
+    </Layout>
+  );
+}
