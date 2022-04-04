16f1e731fd26c5801c1e170a1acb1af1016e2c18
Ezekiel <umanmah234@gmail.com>
2022-04-03 20:37:31 -0400

    sleepslice update reducer add return false

--- frontend/src/reducers/sleepSlice.js
+++ frontend/src/reducers/sleepSlice.js
@@ -14,8 +14,8 @@ export const sleepSlice = createSlice({
       state.data.find((sleep) => {
         if (sleep._id === action.payload._id) {
           Object.assign(sleep, { ...action.payload });
-          return sleep;
         }
+        return false;
       });
     },
     remove: (state, action) => {
