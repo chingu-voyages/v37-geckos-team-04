c526bd50f64970c829772d29afb4364f5f6db8fa
Ezekiel <umanmah234@gmail.com>
2022-04-06 20:54:11 -0400

    add alt attr to image

--- frontend/src/Components/NotFound404.js
+++ frontend/src/Components/NotFound404.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import not_found from '../Images/not_found.png';
-class NotFoundPage extends React.Component{
-    render(){
-        return <div>
-            <p style={{textAlign:"center"}}>
-                <h1>Oops! You followed a wrong link!</h1>
-            </p>
-            <img src={not_found} width="50%" height="50%" />
-            <Link to="/">Go Home </Link>
-          </div>;
-    }
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p style={{ textAlign: 'center' }}>
+          <h1>Oops! You followed a wrong link!</h1>
+        </p>
+        <img
+          alt="not found component"
+          src={not_found}
+          width="50%"
+          height="50%"
+        />
+        <Link to="/">Go Home </Link>
+      </div>
+    );
+  }
 }
 export default NotFoundPage;
