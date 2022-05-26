d11520fbe3b72d94cd46d94660df51123a88a7fd
Ezekiel <umanmah234@gmail.com>
2022-05-26 17:52:57 -0400

    add history table columns

---
+++ frontend/src/Components/09-History/columns.js
@@ -0,0 +1,51 @@
+const columns = [
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    editable: true,
+  },
+  {
+    title: 'Sleep Time',
+    dataIndex: 'sleepStart',
+    editable: true,
+  },
+  {
+    title: 'Wake Time',
+    dataIndex: 'sleepEnd',
+    editable: true,
+  },
+  {
+    title: 'Sleep Mood',
+    dataIndex: 'moodStart',
+    editable: true,
+  },
+  {
+    title: 'Wake Mood',
+    dataIndex: 'moodWake',
+    editable: true,
+  },
+  {
+    title: 'Sleep Goal',
+    dataIndex: 'sleepGoal',
+    editable: true,
+  },
+  {
+    title: 'Sleep Duration',
+    dataIndex: 'sleepDuration',
+  },
+  {
+    title: 'Sleep Notes',
+    dataIndex: 'notesSleep',
+    editable: true,
+  },
+  {
+    title: 'Wake Notes',
+    dataIndex: 'notesWake',
+    editable: true,
+  },
+  {
+    dataIndex: 'Edit',
+  },
+];
+
+export default columns;
