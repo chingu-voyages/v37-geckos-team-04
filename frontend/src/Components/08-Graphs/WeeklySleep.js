30317f65e75c4c652c9b90aa77c68cf6f40306c9
Ezekiel <umanmah234@gmail.com>
2022-06-01 12:42:13 -0400

    reformat/sort data, xAxis/labels

--- frontend/src/Components/08-Graphs/WeeklySleep.js
+++ frontend/src/Components/08-Graphs/WeeklySleep.js
@@ -1,44 +1,62 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import GraphTemplate from './GraphTemplate';
 import { WeeklySleepCont } from './style';
 
+const days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
 export default function WeeklySleep() {
-  // data should be fetched from the user's sleep data from the past week
-  // 'uv' should be fetched from the backend, just for those past 7 days
-  // 'pv' isn't necessary for our needs here
+  const data = useSelector((state) => state.sleepData.data);
 
-  const data = [
-    { name: 'Sunday', uv: 8.7, pv: 0, amt: 24 },
-    { name: 'Monday', uv: 7.4, pv: 0, amt: 24 },
-    { name: 'Tuesday', uv: 6.9, pv: 0, amt: 24 },
-    { name: 'Wednesday', uv: 7.1, pv: 0, amt: 24 },
-    { name: 'Thursday', uv: 7.3, pv: 0, amt: 24 },
-    { name: 'Friday', uv: 6.2, pv: 0, amt: 24 },
-    { name: 'Saturday', uv: 9.4, pv: 0, amt: 24 },
-  ];
+  const dataArr = [];
 
-  const CustomToolTip = ({ active, payload, label }) => {
+  // copy all sleep instances and sort oldest to newest
+  let newData = data.slice();
+  newData = newData.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  // get last 7 sleep instances (may be gaps in between days if user misses recording)
+  newData = newData.slice(-7);
+  // add *name* for x-axis label and *uv* for sleepDuration in hours
+  newData.forEach((sleep, i) => {
+    const update = {};
+    Object.assign(update, sleep);
+    update.name = new Date(sleep.date).getDay();
+    const duration = Math.abs(
+      new Date(sleep.sleepStart) - new Date(sleep.sleepEnd)
+    );
+    update.uv = (duration / 3600000).toFixed(2);
+    dataArr.push(update);
+  });
+
+  const CustomXAxisTick = (payload) => {
+    return days[payload];
+  };
+
+  const CustomToolTip = ({ active, payload }) => {
     if (active && payload && payload.length) {
       return (
         <div className="custom-tooltip">
           <div className="label">
-            {/* <b>{label}</b>: {payload[0].value} hours */}
-            {payload[0].value} hours
+            {new Date(payload[0].payload.date).toDateString()} <br />
+            {<b>{payload[0].payload.sleepDuration}</b>}
           </div>
         </div>
       );
-    } else return null;
+    }
+    return null;
   };
 
   return (
     <WeeklySleepCont>
       <GraphTemplate
-        // width={750}
         height={400}
-        title={'Weekly average daily sleep duration'}
-        data={data}
-        yAxis={'Hours on average'}
+        title={'Hours of sleep per day for the last seven days'}
+        data={dataArr}
+        yAxis={'Hours'}
+        yDomain={[0, (dataMax) => Math.ceil(dataMax * 1.5)]}
         customTooltip={CustomToolTip}
+        CustomXAxisTick={CustomXAxisTick}
       />
     </WeeklySleepCont>
   );
