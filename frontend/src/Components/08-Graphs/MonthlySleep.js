62912a6afede351ff0f8b08f0c3c2fe9d72fb7e4
Ezekiel <umanmah234@gmail.com>
2022-06-05 19:25:42 -0400

    add template for other data averages

--- frontend/src/Components/08-Graphs/MonthlySleep.js
+++ frontend/src/Components/08-Graphs/MonthlySleep.js
@@ -3,9 +3,25 @@ import { useSelector } from 'react-redux';
 import GraphTemplate from './GraphTemplate';
 import { MonthlySleepCont } from './style';
 
-export default function MonthlySleep() {
-  const data = useSelector((state) => state.sleepData.data);
+import mockSleepData from '../09-History/mockSleepData';
 
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+];
+
+export default function MonthlySleep() {
+  // const data = useSelector((state) => state.sleepData.data);
+  const data = mockSleepData;
   // group all sleep data by month
   const formatData = {};
   data.reduce((accu, curr) => {
@@ -27,28 +43,46 @@ export default function MonthlySleep() {
   const graphData = [];
   for (const month in formatData) {
     let meanDuration = 0;
-    const date = new Date(formatData[month][0].date);
-    const key = date.getMonth() + 1 + '/' + date.getFullYear();
+    let meanSleepStart = 0;
+    let meanSleepEnd;
+    let meanMoodStart;
+    let meanMoodEnd;
+
+    const name = new Date(formatData[month][0].date).toLocaleString('default', {
+      month: 'short',
+      year: 'numeric',
+    });
 
     formatData[month].forEach((sleep) => {
-      const duration =
-        Math.abs(new Date(sleep.sleepStart) - new Date(sleep.sleepEnd)) /
-        3600000;
+      // prettier-ignore
+      const duration = Math.abs(new Date(sleep.sleepStart) - new Date(sleep.sleepEnd)) / 3600000;
       meanDuration += duration;
+
+      let sleepStart = new Date(sleep.sleepStart);
+      sleepStart.setFullYear(2020, 1, 1);
+      sleepStart = sleepStart.getTime();
+      meanSleepStart += sleepStart;
     });
 
     meanDuration = (meanDuration / formatData[month].length).toFixed(2);
+    meanSleepStart = new Date(
+      meanSleepStart / formatData[month].length
+    ).toLocaleTimeString();
 
-    graphData.push({ name: key, uv: meanDuration });
+    graphData.push({ name, uv: meanDuration, meanSleepStart });
   }
 
   const CustomToolTip = ({ active, payload, label }) => {
+    console.log(payload);
     if (active && payload && payload.length) {
       return (
         <div className="custom-tooltip">
           <div className="label">
-            {/* <b>{label}</b>: {payload[0].value} hours */}
-            {payload[0].value} hours
+            <b>{label}</b>
+            <br />
+            <b>Average hours of sleep:</b> {payload[0].value} hours
+            <br />
+            <b>Average sleep time:</b> {payload[0].payload.meanSleepStart}
           </div>
         </div>
       );
@@ -61,7 +95,7 @@ export default function MonthlySleep() {
       <GraphTemplate
         // width={750}
         height={400}
-        title={'Monthly average daily sleep duration'}
+        title={'Monthly averages'}
         data={graphData}
         yAxis={'Hours on average'}
         customTooltip={CustomToolTip}
