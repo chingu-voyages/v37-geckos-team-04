c2e6a7bf6327d2463af6bb3b34190fbef2cd0c4c
Ezekiel <umanmah234@gmail.com>
2022-06-19 02:48:47 -0400

    reformat menu item syntax

--- frontend/src/Components/06-Dashboard/Dashboard.js
+++ frontend/src/Components/06-Dashboard/Dashboard.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Menu, Input, Button } from 'antd';
+import { Layout, Menu } from 'antd';
 import {
-  DesktopOutlined,
   PieChartOutlined,
-  DashboardOutlined,
   HistoryOutlined,
   // EyeOutlined,
   // EyeInvisibleOutlined,
@@ -12,6 +10,7 @@ import {
 } from '@ant-design/icons';
 import { Link, Outlet } from 'react-router-dom';
 
+import { SiderContainer } from './style';
 import Modal from '../07-Modal/Modal';
 // import History from '../09-History/History';
 // import Temp from '../08-Graphs/Temp';
@@ -24,13 +23,11 @@ import { logOut } from '../../reducers/sleepSlice';
 import { getSleepData } from '../../reducers/Sleep';
 import mockSleepData from '../../__mocks__/mockSleepData';
 
-const { Content, Sider } = Layout;
-const { SubMenu } = Menu;
+const { Content } = Layout;
 
 export default function Dashboard() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [collapsed, setCollapsed] = useState(false);
   const [isMock, setIsMock] = useState(false);
   const sleepData = useSelector((state) => state.sleepData.data);
 
@@ -48,46 +45,57 @@ export default function Dashboard() {
     navigate('/');
   };
 
+  function getItem(label, key, icon, children) {
+    console.log(label, key, icon, children);
+    return {
+      label,
+      key,
+      icon,
+      children,
+    };
+  }
+
+  const items = [
+    getItem(
+      'Dashboard',
+      'graphs',
+      <Link to="graphs">
+        <PieChartOutlined />
+      </Link>
+    ),
+    getItem(
+      'History',
+      'history',
+      <Link to="history">
+        <HistoryOutlined />
+      </Link>
+    ),
+    getItem(
+      isMock ? 'Unmock Data' : 'Mock Data',
+      'mock data',
+      <Link to="#" onClick={() => setIsMock((prev) => !prev)}>
+        <FormOutlined />
+      </Link>
+    ),
+    getItem(
+      'Logout',
+      'logout',
+      <Link to="/" onClick={logOutUser}>
+        <ArrowRightOutlined />
+      </Link>
+    ),
+  ];
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(collapsed) => setCollapsed(collapsed)}
-        breakpoint="lg"
-        collapsedWidth="0"
-      >
-        <Menu theme="dark" defaultSelectedKeys={['graphs']} mode="inline">
-          <Menu.Item key="graphs" icon={<PieChartOutlined />}>
-            <Link to="graphs">Dashboard</Link>
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<DesktopOutlined />} title="Preferences">
-            <Menu.Item key="2" icon={<DashboardOutlined />}>
-              SleepGoal <Input min={4} max={12} defaultValue={8}></Input>
-            </Menu.Item>
-            {/* <Menu.Item key="3" icon={<EyeInvisibleOutlined />}>Bed-Time</Menu.Item>
-              <Menu.Item key="4" icon={<EyeOutlined />}>Wake-Time</Menu.Item> */}
-          </SubMenu>
-          <Menu.Item key="history" icon={<HistoryOutlined />}>
-            <Link to="history">History</Link>
-          </Menu.Item>
-          <Menu.Item
-            key="mock data"
-            icon={<FormOutlined />}
-            onClick={() => setIsMock((prev) => !prev)}
-          >
-            <Button
-              style={{ padding: 0, color: 'rgba(255, 255, 255, 0.65)' }}
-              type="text"
-            >
-              {isMock ? 'Unmock Data' : 'Mock Data'}
-            </Button>
-          </Menu.Item>
-          <Menu.Item key="6" icon={<ArrowRightOutlined />} onClick={logOutUser}>
-            Log Out
-          </Menu.Item>
-        </Menu>
-      </Sider>
+      <SiderContainer breakpoint="md" collapsedWidth="0">
+        <Menu
+          items={items}
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['graphs']}
+        />
+      </SiderContainer>
       <Layout className="site-layout">
         <Modal id={id} />
         <Content style={{ margin: '25px 35px' }}>
