7e09d0bf93a8d8273a39f558df66682a6fbbf9bd
Ezekiel <umanmah234@gmail.com>
2022-06-19 02:49:34 -0400

    add Sider styles for mobile screen

--- frontend/src/Components/06-Dashboard/style.js
+++ frontend/src/Components/06-Dashboard/style.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+import { Layout } from 'antd';
+const { Sider } = Layout;
+
+export const SiderContainer = styled(Sider)`
+  @media screen and (max-width: 768px) {
+    position: fixed;
+    z-index: 99;
+    height: 100vh;
+  }
+`;
