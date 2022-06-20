bd1888a06e241abf804a0ec980a30c68cde6a93f
Ezekiel <umanmah234@gmail.com>
2022-06-20 11:50:25 -0400

    refactor markup and inline styles

--- frontend/src/Components/08-Graphs/GraphContainer.js
+++ frontend/src/Components/08-Graphs/GraphContainer.js
@@ -8,28 +8,26 @@ import DayOfTheWeek from './DayOfTheWeek';
 export default function GraphContainer() {
   const data = useOutletContext();
   return (
-    <div style={{ padding: 24, minHeight: 360 }}>
-      <div className="site-card-wrapper">
-        <Row gutter={[5, 5]}>
-          <Col flex="auto">
-            <Card style={{ minHeight: '30vh' }}>
-              <WeeklySleep data={data} />
-            </Card>
-          </Col>
-          <Col flex="auto">
-            <Card style={{ minHeight: '30vh' }}>
-              <MonthlySleep data={data} />
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={15} style={{ marginTop: '25px' }}>
-          <Col flex="auto">
-            <Card style={{ minHeight: '40vh' }}>
-              <DayOfTheWeek data={data} />
-            </Card>
-          </Col>
-        </Row>
-      </div>
+    <div>
+      <Row gutter={[5, 5]}>
+        <Col flex="auto">
+          <Card>
+            <WeeklySleep data={data} />
+          </Card>
+        </Col>
+        <Col flex="auto">
+          <Card>
+            <MonthlySleep data={data} />
+          </Card>
+        </Col>
+      </Row>
+      <Row gutter={15}>
+        <Col flex="auto">
+          <Card style={{ minHeight: '40vh' }}>
+            <DayOfTheWeek data={data} />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
