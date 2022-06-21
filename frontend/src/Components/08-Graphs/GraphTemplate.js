d31e948f37cfaafc59df9ad5ec93930852b4ecef
Ezekiel <umanmah234@gmail.com>
2022-06-21 09:22:04 -0400

    conditionally render margins + yLabels

--- frontend/src/Components/08-Graphs/GraphTemplate.js
+++ frontend/src/Components/08-Graphs/GraphTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ComposedChart,
   Line,
@@ -23,14 +23,31 @@ export default function GraphTemplate(props) {
     CustomXAxisTick,
   } = props;
 
+  const [yLabel, setYLabel] = useState({ value: yAxis, angle: -90 });
+  const [margin, setMargin] = useState({
+    top: 0,
+    right: 10,
+    left: 5,
+    bottom: 0,
+  });
+
+  useEffect(() => {
+    const viewportWidth = window.innerWidth;
+
+    if (viewportWidth < 576) {
+      setYLabel((prev) => ({ ...prev, value: null }));
+      setMargin((prev) => ({ ...prev, left: -25 }));
+    }
+  }, []);
+
   return (
     <GraphTemplateCont>
       <h1 className="title">{title}</h1>
       <ResponsiveContainer width="100%" height={height}>
-        <ComposedChart data={data}>
+        <ComposedChart data={data} margin={margin}>
           <CartesianGrid stroke="#f5f5f5" />
           <XAxis dataKey="name" tickFormatter={CustomXAxisTick} interval={0} />
-          <YAxis label={{ value: yAxis, angle: -90 }} domain={yDomain} />
+          <YAxis label={yLabel} domain={yDomain} />
           <Tooltip content={customTooltip} />
           <Bar dataKey="uv" barSize={70} fill="#cdcdcd" />
           <Line type="monotone" dataKey="uv" stroke="#413ea0" strokeWidth={2} />
