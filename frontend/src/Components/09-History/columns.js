e7a36656c410fb2419fa2a3daa97687aafccfbb3
Ezekiel <umanmah234@gmail.com>
2022-06-21 10:26:17 -0400

    make columns responsive

--- frontend/src/Components/09-History/columns.js
+++ frontend/src/Components/09-History/columns.js
@@ -8,40 +8,48 @@ const columns = [
     title: 'Sleep Time',
     dataIndex: 'sleepStart',
     editable: true,
+    responsive: ['md'],
   },
   {
     title: 'Wake Time',
     dataIndex: 'sleepEnd',
     editable: true,
+    responsive: ['md'],
   },
   {
     title: 'Sleep Mood',
     dataIndex: 'moodStart',
     editable: true,
+    responsive: ['lg'],
   },
   {
     title: 'Wake Mood',
     dataIndex: 'moodWake',
     editable: true,
+    responsive: ['lg'],
   },
   {
     title: 'Sleep Goal',
     dataIndex: 'sleepGoal',
     editable: true,
+    responsive: ['lg'],
   },
   {
     title: 'Sleep Duration',
     dataIndex: 'sleepDuration',
+    responsive: ['lg'],
   },
   {
     title: 'Sleep Notes',
     dataIndex: 'notesSleep',
     editable: true,
+    responsive: ['lg'],
   },
   {
     title: 'Wake Notes',
     dataIndex: 'notesWake',
     editable: true,
+    responsive: ['lg'],
   },
   {
     dataIndex: 'Edit',
