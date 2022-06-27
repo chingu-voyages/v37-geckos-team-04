41600803197f670b20ae1b3f65a2f1cde1763843
Ezekiel <umanmah234@gmail.com>
2022-06-27 17:30:48 -0400

    Reformat styles for Dashboard header

--- frontend/src/Components/06-Dashboard/style.js
+++ frontend/src/Components/06-Dashboard/style.js
@@ -13,10 +13,21 @@ export const SiderContainer = styled(Sider)`
 export const Header = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  margin: 1em;
+  padding: 1em;
+  background-color: rgb(214, 214, 214);
+  -webkit-box-shadow: 0px 0px 14px -2px rgba(0, 0, 0, 0.48);
+  -moz-box-shadow: 0px 0px 14px -2px rgba(0, 0, 0, 0.48);
+  box-shadow: 0px 0px 14px -2px rgba(0, 0, 0, 0.48);
 
   h1 {
     font-size: 1.75em;
+    letter-spacing: 1.5px;
+    font-weight: 300;
+  }
+
+  @media screen and (max-width: 577px) {
+    h1 {
+      font-size: 1.25em;
+    }
   }
 `;
